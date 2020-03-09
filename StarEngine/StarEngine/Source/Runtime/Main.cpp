4d520c809aeaa3db8ea07761e8b6ad71fc2c1f71
FreeStar <623243400@qq.com>
2020-03-09 17:37:17 +0800

    Filter boost.zip

--- StarEngine/StarEngine/Source/Runtime/Main.cpp
+++ StarEngine/StarEngine/Source/Runtime/Main.cpp
@@ -6,15 +6,13 @@
 #include "../Core/Math/SMath.h"
 //typedef signed int int32;
 
-int main() 
+int main()
 {
-	//SVector3 a(1,2,3);
-	//SVector3 b(3, 2, 1);
-	//SVector3 c = b ^ a;
-
-	//std::cout << c.x << std::endl;
-	//std::cout << c.y << std::endl;
-	//std::cout << c.z << std::endl;
+	SVector3 a(1, 2, 3);
+	SVector3 b(3, 2, 1);
+	SVector3 c = b ^ a;
+	std::cout << c.x << std::endl;
+	std::cout << c.y << std::endl;
+	std::cout << c.z << std::endl;
 	return 0;
 }
-
